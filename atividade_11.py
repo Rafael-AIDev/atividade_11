b0243747c0f5f1b42a42e87b44a0ba3213839914
Rafael-AIDev <rafael.it.ai.br@gmail.com>
2024-08-02 17:28:33 -0300

    Lista 1.0

---
+++ atividade_11.py
@@ -0,0 +1,8 @@
+'''Crie um programa que imprima uma lista com todos os estados do Brasil.
+ Ao terminar, crie um repositório, envie para o GitHub e poste o link do repositório remoto.'''
+
+# lista
+estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Rorarima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
+
+for i in range (27):
+    print(f'{i +1}º estado: {estados[i]}.')
